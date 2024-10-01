c182c19390f61f8d27d643bcc6aa7c630cfa48aa
Osama eid <osamaeidbm1993@gmail.com>
2024-10-01 12:07:54 +0300

    ok

--- resources/views/admin/body/top_nav.blade.php
+++ resources/views/admin/body/top_nav.blade.php
@@ -135,7 +135,7 @@
         <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
             <!--begin::Menu wrapper-->
             <div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                <img src="{{asset('assets/media/avatars/300-3.jpg')}}" class="rounded-3" alt="user" />
+                <img src="{{ auth()->user()->photo ? asset('/upload/admin_images/' . auth()->user()->photo) : asset('assets/avatar.png') }}" class="rounded-3" alt="user" />
             </div>
             <!--begin::User account menu-->
             <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -144,7 +144,7 @@
                     <div class="menu-content d-flex align-items-center px-3">
                         <!--begin::Avatar-->
                         <div class="symbol symbol-50px me-5">
-                            <img alt="Logo" src="{{asset('assets/media/avatars/300-3.jpg')}}" />
+                            <div class="user-photo" style="background-image: url('{{ auth()->user()->photo ? asset('/upload/admin_images/' . auth()->user()->photo) : asset('assets/avatar.png') }}'); width: 50px; height: 50px; background-size: cover; background-position: center;"></div>
                         </div>
                         <!--end::Avatar-->
                         <!--begin::Username-->
