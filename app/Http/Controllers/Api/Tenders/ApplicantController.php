60b4de4380f0436f0d8d4c3ea67c252a2a1bb9c9
Osama eid <osamaeidbm1993@gmail.com>
2024-10-21 12:48:35 +0300

    wow

--- app/Http/Controllers/Api/Tenders/ApplicantController.php
+++ app/Http/Controllers/Api/Tenders/ApplicantController.php
@@ -57,28 +57,19 @@ class ApplicantController extends Controller
     }
     public function update(Request $request, $id)
     {
-        // Validate the request data
-        $validatedData = $request->validate([
-            'file' => 'nullable|file|mimes:pdf,doc,docx,jpg,png|max:2048', // Make file optional for update
-        ]);
+ 
     
-        // Find the application by ID
         $application = Applicant::findOrFail($id);
     
-        // Convert edit_end_date to a Carbon instance
         $editEndDate = Carbon::parse($application->edit_end_date);
     
-        // Check if the current time is before the edit_end_date
         if (now()->lessThan($editEndDate)) {
             
-            // Handle file upload if a new file is provided
         if ($request->hasFile('file')) {
-            // Store the new file and update the file path
             $filePath = $request->file('file')->store('applications', 'public');
-            $application->files = $filePath; // Update the file path
+            $application->files = $filePath; 
         }
     
-        // Update other application details if necessary
         $application->save(); // Save the application with the updated fields
     
         return response()->json([
