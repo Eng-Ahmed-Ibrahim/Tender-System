ed20be72b7d0325bdd4354b608f909b774ad3ca9
Osama eid <osamaeidbm1993@gmail.com>
2024-10-21 10:31:45 +0300

    done works

--- app/Http/Controllers/Api/Profile/ProfileController.php
+++ app/Http/Controllers/Api/Profile/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Profile;
 
 use Storage;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
