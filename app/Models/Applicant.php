04537351b5766e929d01a798008cbbc37827141b
Osama eid <osamaeidbm1993@gmail.com>
2024-10-26 16:46:44 +0300

    wow

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Applicant extends Model
 {
     use HasFactory;
 
     protected $guarded=[];
+
+
+
+    public function user(){
+
+        return belongsTo(User::class,'user_id');
+    }
 
 
 
